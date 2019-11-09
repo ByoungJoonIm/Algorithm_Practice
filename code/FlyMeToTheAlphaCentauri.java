49e85b111680897e4a0e042e32af560c39cde715
ByoungJoonIm <43159254+ByoungJoonIm@users.noreply.github.com>
2019-11-09 15:55:00 -0500

    Add files via upload

---
+++ code/FlyMeToTheAlphaCentauri.java
@@ -0,0 +1,25 @@
+package mathmatics;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class FlyMeToTheAlphaCentauri {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			bw.write((int)Math.ceil(2 * Math.sqrt(y - x) - 1) + "\n");
+		}
+		br.close();
+		bw.close();
+	}
+}
