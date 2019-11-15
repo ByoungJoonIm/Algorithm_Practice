12b44eed2d83b9dd764552841fd7f8680f0cb9e7
ByoungJoonIm <43159254+ByoungJoonIm@users.noreply.github.com>
2019-11-15 15:35:37 -0500

    Update LongestIncreasingSub.java

--- code/LongestIncreasingSub.java
+++ code/LongestIncreasingSub.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
-//¹Ý·Ê Ã£´ÂÁß
 public class LongestIncreasingSub {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
