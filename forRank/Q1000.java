fba66028f7149885e3a5c9d9c6ba98d90a3bf4af
ByoungJoonIm <43159254+ByoungJoonIm@users.noreply.github.com>
2020-01-11 13:56:19 +0900

    Add files via upload

---
+++ forRank/Q1000.java
@@ -0,0 +1,23 @@
+package forRank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q1000 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		bw.write((A + B) + "\n");
+		
+		br.close();
+		bw.close();
+	}
+}
