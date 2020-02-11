d547428400e8defcc71e5a7e7849e1473172421a
ByoungJoonIm <fortress0427@gmail.com>
2020-02-11 12:59:50 +0900

    Add files via upload

---
+++ forRank/Q11048.java
@@ -0,0 +1,29 @@
+package forRank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q11048 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int dp[][] = new int[N + 1][M + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= M; j++)
+				dp[i][j] = Integer.parseInt(st.nextToken()) + Math.max(dp[i - 1][j], dp[i][j - 1]);
+		}
+		bw.write(dp[N][M] + "\n");
+		
+		br.close();
+		bw.close();
+	}
+}
