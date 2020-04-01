6af682f67f3955bbfc59f09aed0e44b0473f86d6
ByoungJoonIm <fortress0427@gmail.com>
2020-04-01 17:15:07 +0900

    Add files via upload

---
+++ forRank/Q5063.java
@@ -0,0 +1,33 @@
+package forRank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q5063 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			if(r == e - c)
+				bw.write("does not matter\n");
+			else if(r < e - c)
+				bw.write("advertise\n");
+			else
+				bw.write("do not advertise\n");
+		}
+		
+		br.close();
+		bw.close();
+	}
+}
