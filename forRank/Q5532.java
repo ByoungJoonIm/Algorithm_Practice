786aae8f34c6b0e400c37423834e4a7fe1caf022
ByoungJoonIm <fortress0427@gmail.com>
2020-04-03 11:10:39 +0900

    Add files via upload

---
+++ forRank/Q5532.java
@@ -0,0 +1,25 @@
+package forRank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q5532 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int L = Integer.parseInt(br.readLine());
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		int D = Integer.parseInt(br.readLine());
+		
+		bw.write((L - Math.max((A - 1) / C + 1, (B - 1) / D + 1)) + "\n");
+		
+		br.close();
+		bw.close();
+	}
+}
