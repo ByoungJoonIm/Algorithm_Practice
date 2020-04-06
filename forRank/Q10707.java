513f86355b5552c56b37744d377125fc006dfcd0
ByoungJoonIm <fortress0427@gmail.com>
2020-04-06 16:58:30 +0900

    Add files via upload

---
+++ forRank/Q10707.java
@@ -0,0 +1,25 @@
+package forRank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q10707 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		int D = Integer.parseInt(br.readLine());
+		int P = Integer.parseInt(br.readLine());
+		
+		bw.write(Math.min(A * P, (P > C) ? (P - C) * D + B : B) + "\n");
+		
+		br.close();
+		bw.close();
+	}
+}
