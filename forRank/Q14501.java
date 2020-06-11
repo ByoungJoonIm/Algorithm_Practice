f6aafc016c3b4e90e87cd7bab16af5ab09af1cf9
byoungjoonim <bj.max.lim@gmail.com>
2020-06-12 08:48:54 +0900

    Add files via upload

--- forRank/Q14501.java
+++ forRank/Q14501.java
@@ -8,7 +8,7 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Q14501 {
-	public static void main(String[] args) throws IOException {
+	private static void solution1() throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N  = Integer.parseInt(br.readLine());
@@ -35,5 +35,44 @@ public class Q14501 {
 		
 		br.close();
 		bw.close();
+
+	}
+	
+	private static void solution2() throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int T[] = new int[N + 1];
+		int P[] = new int[N + 1];
+		int dp[] = new int[N + 2];
+		
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int maxValue = 0;
+		
+		for (int i = 1; i <= N; i++) {
+			int nextDay = i + T[i];
+			maxValue = Math.max(maxValue, dp[i]);
+			dp[i] = maxValue;
+			
+			if(nextDay <= N + 1){
+				dp[nextDay] = Math.max(dp[nextDay], dp[i] + P[i]);
+			}
+		}
+		
+		bw.write(Math.max(maxValue, dp[N + 1]) + "\n");
+		
+		br.close();
+		bw.close();
+	}
+	
+	public static void main(String[] args) throws IOException {
+//		solution1();
+		solution2();
 	}
 }
