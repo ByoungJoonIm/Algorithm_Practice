642f6cf9d1c41834e88b2a47431d699baf481a84
byoungjoonim <bj.max.lim@gmail.com>
2020-06-26 08:48:06 +0900

    Add files via upload

---
+++ forRank/Q15663.java
@@ -0,0 +1,72 @@
+package forRank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Q15663 {
+	private static int N, M;
+	private static int selected[];
+	private static int numbers[];
+	private static StringBuilder sb;
+	private static HashSet<String> hs;
+	
+	private static void dfs(int depth, boolean visited[]){
+		if(depth == M){
+			StringBuilder temp = new StringBuilder();
+			
+			for (int i = 0; i < M - 1; i++) {
+				temp.append(selected[i]).append(' ');
+			}
+			temp.append(selected[M - 1]).append('\n');
+			
+			if(!hs.contains(temp.toString())){
+				sb.append(temp.toString());
+				hs.add(temp.toString());
+			}
+			
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if(!visited[i]){
+				visited[i] = true;
+				selected[depth] = numbers[i];
+				dfs(depth + 1, visited);
+				visited[i] = false;
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		hs = new HashSet<String>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		selected = new int[M];
+		numbers = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(numbers);
+		dfs(0, new boolean[N]);
+		
+		bw.write(sb.toString());
+		
+		br.close();
+		bw.close();
+	}
+}
